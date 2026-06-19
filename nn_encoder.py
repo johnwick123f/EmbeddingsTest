a127f3c4721144296c65ba820cadadcff3c47259
johnwick123f <128541091+johnwick123f@users.noreply.github.com>
2026-06-19 17:56:53 -0400

    Add UniformQuantizer import to nn_encoder.py

--- nn_encoder.py
+++ nn_encoder.py
@@ -5,6 +5,7 @@ import torch.optim as optim
 from torch.utils.data import DataLoader, TensorDataset
 from tqdm import tqdm
 from scipy.cluster.vq import kmeans2
+from quantizer import UniformQuantizer
 
 # --- Official ConvNeXt V2 Component Integrations ---
 
